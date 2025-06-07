28a9cdad64015807ed38cd7e710137fe550dcc62
Thenguyen67 <itachim905@gmail.com>
2025-06-07 19:47:09 +0700

    Bai 2 Buoi 1

--- bai2_1.cpp
+++ bai2_1.cpp
@@ -10,4 +10,5 @@ int main()
     if ( x > pow(10 , 5) )
     cout << "So a khong hop le ! , vui long nhap lai" ;
     cout <<"A(x) = x^3 + 3x^2 + x + 1 = " << pow(x , 3) + 3 * pow ( x , 2 ) + x + 1 ;
+    return 0 ;
 }
