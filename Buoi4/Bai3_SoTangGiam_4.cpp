46753541da92d234792d24978d32c8fe9d3cf8ff
Thenguyen67 <itachim905@gmail.com>
2025-07-03 01:39:48 +0700

    Update Bai3_SoTangGiam_4.cpp

--- Buoi4/Bai3_SoTangGiam_4.cpp
+++ Buoi4/Bai3_SoTangGiam_4.cpp
@@ -50,34 +50,5 @@ int main()
     cin >> n ;
     int a = n - 1 , b = 1 ;
     int SoLuong = 0 ;
-<<<<<<< HEAD:Buoi4/Bai3_SoTangGiam_4.cpp
-    //  while(a > 0)
-    // {
-    //     a = a - 1 ;
-    //     b = b * 10 ;
-    // }
-    // for(int i = b ; i <= b*10 - 1 ; i++)
-    // {
-    //     int SoLuong = 0 ;
-    //     bool c ;
-    //     if(i > 2)
-    //     {
-    //         c = true ;
-    //         for(int j = 2 ; j*j <= i ; j++)
-    //         {
-    //             if(i % j == 0) c = false ;
-    //         }
-    //         if(c) //1
-    //         {
-    //             int d = n % 10 ;
-    //             int e = n / 10 ;
-    //             if (d < e || d > e) SoLuong++ ;
-    //         }
-    //     }
-    // }
     SoLuongPhanTu(a , b) ;
 }
-=======
-    SoLuongPhanTu(a , b , SoLuong) ;
-}
->>>>>>> 891bd6d (1):Bai3_SoTangGiam_4.cpp
