ac7616c717735576c001ad0667971f9d3126bd1a
Thenguyen67 <itachim905@gmail.com>
2025-08-16 21:03:00 +0700

    1

---
+++ DLL_Add_Print_Basic.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std ;
+
+struct Node
+{
+    int data ;
+    Node* prev ;
+    Node* next ;
+};
+
+Node* CreateNode(int value)
+{
+    Node* a = new Node() ;
+    a->data = value ;
+    a->prev = nullptr ;
+    a->next = nullptr ;
+    return a ;
+}
+
+void AddNode(Node* &head , int value)
+{
+    Node* newNode = CreateNode(value) ;
+    if(head == nullptr)
+    {
+        head = newNode ;
+        return ;
+    }
+
+    Node* temp = head ;
+    while(temp->next != nullptr)
+    {
+        temp = temp->next ;
+    }
+    temp->next = newNode ;
+    newNode->prev = temp ;
+}
+
+void PrintList(Node* head)
+{
+    cout << endl << "Danh sach phan tu : " ;
+    Node* temp = head ;
+    while(temp != nullptr)
+    {
+        cout << temp->data << " " ;
+        temp = temp->next ;
+    }
+    cout << endl << endl ;
+}
+
+int main()
+{
+    Node* head = nullptr ;
+    int value , choice ;
+    
+    do 
+    {
+        cout << "1. Nhap phan tu" << endl ;
+        cout << "2. In danh sach" << endl ;
+        cout << "0. Ket thuc chuong trinh" << endl ;
+        cout << "Lua chon : " ;
+        cin >> choice ;
+        switch(choice)
+        {
+            case 1 :
+                cout << "Nhap gia tri : " ;
+                cin >> value ;
+                cout << endl ;
+                AddNode(head , value) ;
+                break ;
+            case 2 :
+                PrintList(head) ;
+                break ;
+            case 0 : 
+                cout << "Ket thuc danh sach" << endl ;
+                break ;
+            default : 
+                cout << "Nhap sai ! Vui long nhap lai" << endl ;
+        }
+    }
+    while(choice != 0) ;
+}
